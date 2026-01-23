875a2a90b8ed01ca1e0f2afed8f201da7d12c59e
yktham <yongkintham@gmail.com>
2026-01-23 13:41:18 +0800

    assignment 6

--- lesson6/root.js
+++
@@ -1,9 +0,0 @@
-const domContainer = document.getElementById("root");
-const root = ReactDOM.createRoot(domContainer);
-
-root.render(
-  <div>
-    <p>Welcome Tom to Singapore!</p>
-    <p>Welcome Jerry to Singapore!</p>
-  </div>
-);
