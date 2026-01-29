02ae2d76891c3e505293290d14c6937542a43cb3
yktham <yongkintham@gmail.com>
2026-01-29 17:54:39 +0800

    lesson 7

---
+++ lesson7/currency-converter/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  //function handleOnMouseEnter(){
+  //  alert("you have hovered over this button")
+  //}
+    
+
+  return (
+    <>
+      <h1>Currency Converter</h1>
+      <select>
+        <option> USD</option>
+        <option> SGD</option>
+        <option> MYR</option>
+      </select>
+    </>
+  )
+}
+
+export default App;
