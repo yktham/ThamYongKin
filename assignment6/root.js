6f4e6d9afad44474a081ca05bfc02f1d11be506a
yktham <yongkintham@gmail.com>
2026-02-05 21:36:28 +0800

    assignment 6

--- assignment6/root.js
+++ assignment6/root.js
@@ -1,7 +1,10 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 // root.js
 
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -25,7 +28,10 @@ function Footer() {
 
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 // BlogPost component
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
@@ -40,8 +46,12 @@ function BlogPost({ title, author, date, content, isPrivate }) {
           ? "The content of this post is private"
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
           : content
         }
+=======
+          : content}
+>>>>>>> Stashed changes
 =======
           : content}
 >>>>>>> Stashed changes
@@ -78,7 +88,11 @@ function App() {
       date: "June 1, 2023",
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
       content: "Welcome to my first blog post! Today, I want to share my journey into the world of blogging.",
+=======
+      content: "Welcome to my first blog post! Today, I want to share my journey into the world of blogging. It has been an exciting experience so far, and i cant't wait to see where this path leads me.",
+>>>>>>> Stashed changes
 =======
       content: "Welcome to my first blog post! Today, I want to share my journey into the world of blogging. It has been an exciting experience so far, and i cant't wait to see where this path leads me.",
 >>>>>>> Stashed changes
@@ -93,7 +107,11 @@ function App() {
       date: "June 5, 2023",
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
       content: "In this post, I’m discussing the importance of work-life balance. It's essential to find time for both professional and personal activites to maintain overall well-being.",
+=======
+      content: "In this post, I’m discussing the importance of work-life balance. It's essential to find time for both professional and personal activities to maintain overall well-being.",
+>>>>>>> Stashed changes
 =======
       content: "In this post, I’m discussing the importance of work-life balance. It's essential to find time for both professional and personal activities to maintain overall well-being.",
 >>>>>>> Stashed changes
@@ -108,7 +126,11 @@ function App() {
       date: "June 10, 2023",
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
       content: "Ever wondered what a day in my life looks like? Join me as I walk through my daily routine, from morning cofee to evening relaxation.",
+=======
+      content: "Ever wondered what a day in my life looks like? Join me as I walk you through my daily routine, from morning coffee to evening relaxation",
+>>>>>>> Stashed changes
 =======
       content: "Ever wondered what a day in my life looks like? Join me as I walk you through my daily routine, from morning coffee to evening relaxation",
 >>>>>>> Stashed changes
@@ -123,7 +145,11 @@ function App() {
       date: "June 15, 2023",
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
       content: "Eating healthy doesn’t have to be complicated.",
+=======
+      content: "Eating healthy doesn’t have to be complicated. Here are some simple and practical tips to help you make better food choices every day.",
+>>>>>>> Stashed changes
 =======
       content: "Eating healthy doesn’t have to be complicated. Here are some simple and practical tips to help you make better food choices every day.",
 >>>>>>> Stashed changes
